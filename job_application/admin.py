31b916128c778389f7117de79a2f2d4054156d04
karthik9990 <karthik.tadakapalli@gmail.com>
2024-09-18 11:41:52 -0600

    Added admin page, username and password, filtered out inside by creating a new class FormAdmin

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Form
+
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "date", "occupation")
+    search_fields = ("first_name", "last_name", "email", "date", "occupation")
+    list_filter = ("date", "occupation")
+    ordering = ("-date",)
+    readonly_fields = ("occupation",)
+
+
+
+admin.site.register(Form, FormAdmin)
